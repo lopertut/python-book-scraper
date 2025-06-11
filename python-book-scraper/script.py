343f55dd9f0f695538305f4ddac8f5be068afad8
lopertut <artur.matvejev12345@gmail.com>
2025-06-11 17:52:48 +0300

    initial commit

--- python-book-scraper/script.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-page = requests.get("https://books.toscrape.com/catalogue/page-1.html")
-soup = BeautifulSoup(page.content, 'html.parser')
-
-books_name = soup.find_all("a", "title")
-print(books_name)
-
