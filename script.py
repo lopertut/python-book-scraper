1f71de1826b272f867f69b7797d70479e93a5efe
lopertut <artur.matvejev12345@gmail.com>
2025-06-13 15:57:28 +0300

    made normal writing to xml file and grouped by stock

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-from dict2xml import dict2xml
+from dicttoxml import dicttoxml
+from xml.dom.minidom import parseString
 
 page = requests.get("https://books.toscrape.com/catalogue/page-1.html")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -11,7 +12,6 @@ page_number = int(input("Enter the page number(max 50): "))
 if page_number > 50 or page_number < 0:
     raise ValueError("Page number must be between 1 and 50")
 
-books = {}
 json_books = {
     "five-stars": [],
     "four-stars": [],
@@ -19,14 +19,17 @@ json_books = {
     "two-stars": [],
     "one-stars": []
 }
-xml_books = {}
-index = 1
 
-for i in range(page_number):
+xml_books = {
+    "in stock": [],
+    "out of stock": []
+}
+
+for i in range(page_number + 1):
     for card in book_cards:
         book = {}
-        # TODO: может стоит добавить заходить на страницу книги и уже оттуда брать данные
 
+        # Тут мы извлекаем данные
         h3_tag = card.find("h3")
         a_tag = h3_tag.find("a")
         book['title'] = a_tag['title']
@@ -34,12 +37,13 @@ for i in range(page_number):
         rating = card.find("p")['class'][1]
         book['rating'] = rating
 
-        price = card.find("p", class_="price_color").text.replace("£", "")
+        price = card.find("p", class_="price_color").text.replace("£", "") # TODO: может стоит убрать потом если не будет ругатся
         book['price'] = price
 
         in_stock = card.find("p", class_="instock availability").text.strip()
         book['stock'] = in_stock
 
+        # Сортируем книги по рейтингу для json файла
         if book["rating"] == "Five":
             json_books["five-stars"].append(book)
         elif book["rating"] == "Four":
@@ -51,17 +55,23 @@ for i in range(page_number):
         elif book["rating"] == "One":
             json_books["one-stars"].append(book)
 
-        books[index] = book
-        index += 1
-    page = requests.get(f"https://books.toscrape.com/catalogue/page-{i}.html")
+        # Сортируем книги по наличию на скаладе
+        if book["stock"] == "In stock":
+            xml_books["in stock"].append(book)
+        else:
+            xml_books["out of stock"].append(book)
 
-# TODO: тут надо какую то структуру сделать особеную или для xml тоже
+    page = requests.get(f"https://books.toscrape.com/catalogue/page-{i}.html")  # идем на следующию страницу каталога
+
+# Записываем данные в json файл
 with open('books.json', 'w') as file:
     books_json = json.dumps(json_books, indent=4)
     file.write(books_json)
     file.close()
 
+# Записываем данные в xml файл
 with open('books.xml', 'w') as file:
-    xml = dict2xml(books, wrap="root")
-    file.write(xml)
+    my_item_func = lambda x: 'book'
+    xml = dicttoxml(xml_books, custom_root="books", attr_type=False, item_func=my_item_func)
+    file.write(parseString(xml).toprettyxml())
     file.close()
