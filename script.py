fe32821914afc9e7ecb7c72c34cc331189d09e6d
lopertut <artur.matvejev12345@gmail.com>
2025-06-13 14:06:36 +0300

    grouped books by rating in json

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+from dict2xml import dict2xml
 
 page = requests.get("https://books.toscrape.com/catalogue/page-1.html")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -11,25 +12,44 @@ if page_number > 50 or page_number < 0:
     raise ValueError("Page number must be between 1 and 50")
 
 books = {}
+json_books = {
+    "five-stars": [],
+    "four-stars": [],
+    "three-stars": [],
+    "two-stars": [],
+    "one-stars": []
+}
+xml_books = {}
 index = 1
 
 for i in range(page_number):
     for card in book_cards:
         book = {}
-        # TODO: может стоит добавить заходит на страницу книги и уже оттуда брать данные
+        # TODO: может стоит добавить заходить на страницу книги и уже оттуда брать данные
 
         h3_tag = card.find("h3")
         a_tag = h3_tag.find("a")
         book['title'] = a_tag['title']
 
-        rating = card.find("p")
-        book['rating'] = rating['class'][1]
+        rating = card.find("p")['class'][1]
+        book['rating'] = rating
 
-        price = card.find("p", class_="price_color").text
+        price = card.find("p", class_="price_color").text.replace("£", "")
         book['price'] = price
 
         in_stock = card.find("p", class_="instock availability").text.strip()
         book['stock'] = in_stock
+
+        if book["rating"] == "Five":
+            json_books["five-stars"].append(book)
+        elif book["rating"] == "Four":
+            json_books["four-stars"].append(book)
+        elif book["rating"] == "Three":
+            json_books["three-stars"].append(book)
+        elif book["rating"] == "Two":
+            json_books["two-stars"].append(book)
+        elif book["rating"] == "One":
+            json_books["one-stars"].append(book)
 
         books[index] = book
         index += 1
@@ -37,6 +57,11 @@ for i in range(page_number):
 
 # TODO: тут надо какую то структуру сделать особеную или для xml тоже
 with open('books.json', 'w') as file:
-    books_json = json.dumps(books, indent=4)
+    books_json = json.dumps(json_books, indent=4)
     file.write(books_json)
+    file.close()
+
+with open('books.xml', 'w') as file:
+    xml = dict2xml(books, wrap="root")
+    file.write(xml)
     file.close()
